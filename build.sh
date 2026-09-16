29310f797bb519842eb0a72acb668ddec3b2f8f4
Abel-237 <fotsoabel91@gmail.com>
2026-09-16 12:56:55 +0100

    Prepare project for Render

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input --settings=config.settings.prod
+
+python manage.py migrate --settings=config.settings.prod
